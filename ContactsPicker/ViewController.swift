4a06302f4593e59094b4a7ca6f77f189d70351d5
Idan Moshe <idanmos@gmail.com>
2020-11-07 20:51:14 +0200

    Initial Commit

---
+++ ContactsPicker/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ContactsPicker
+//
+//  Created by Idan Moshe on 07/11/2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
